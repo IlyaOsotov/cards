42f70542b3b4cb7b07fdd2b093e6d42d2b804a67
IlyaOsotov <soten58@yandex.ru>
2019-04-12 12:19:25 +0300

    added suits to cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["One", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    cards = for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
